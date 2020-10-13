97135e7ebec0ba96c50138f2a50ba0968a3379b1
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-13 23:34:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  difference =[]
+  new_hash = {}
+  total = name_hash.sum {|key, value| value}
+  name_hash.each do |k, v| 
+  
 end
+
+key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
