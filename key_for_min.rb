5da7c5a5b737a8bd789f55e4076635026eb561ff
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-13 23:45:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   total = name_hash.sum {|key, value| value}
   name_hash.each do |k, v| 
     name_hash[k] = total - v
-  puts name_hash
   end 
+  puts name_hash
 end
 
 key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
