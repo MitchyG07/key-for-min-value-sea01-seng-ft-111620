ea0ccf0bd4ea98adfce6a8d5bec9ce91ede8e542
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-14 02:11:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,8 +11,8 @@ def key_for_min_value(name_hash)
     name_hash[k] = total - v
   end 
   new_hash = name_hash.max_by{|k, v| v}
-    
-
-end
+  puts new_hash[0]
+  return new_hash [0]
+  end
 end
 key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
