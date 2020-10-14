5cc071d8fef923cbeb9ebc8aa6c3b07848652904
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-14 02:03:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,11 +9,6 @@ def key_for_min_value(name_hash)
     name_hash[k] = total - v
   end 
   new_hash = name_hash.max_by{|k, v| v}
-  if new_hash[0][1].class != Integer 
-    puts 0
-  else 
   puts new_hash[0]
-end 
-end
 
 key_for_min_value({:chair => 25, :table => 85, :mattress => 450})
