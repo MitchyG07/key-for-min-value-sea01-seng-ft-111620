10e4482f3209615eceabf7427e2f50732bfd54e9
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-14 02:00:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     name_hash[k] = total - v
   end 
   new_hash = name_hash.max_by{|k, v| v}
-  if new_hash[0].class != integer 
+  if new_hash[0].class != Integer 
     return nil 
   else 
   p new_hash[0]
